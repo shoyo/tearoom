dd195483f66e81c3d758f0f21d5225ac74f5237b
shoyo-inokuchi <shoyoinokuchi@gmail.com>
2019-01-06 02:19:29 +0900

    prototype complete. still needs documentation.

--- main.py
+++ main.py
@@ -1,35 +1,88 @@
-import pygame
-from pygame.locals import *
+import time
+import tkinter as tk
 from tearoom import Tearoom
 
 
 def main():
-    """Runs the art piece."""
-    teacup_count = 9
-    tearoom = Tearoom(teacup_count)
-    screen_width = 300 / teacup_count
-    screen_height = 300 / teacup_count
+    """Run the art piece."""
+    root = tk.Tk()
+    root.title("Tearoom of Zero/One")
+    root.minsize(735, 525)
+    root.geometry("735x525")
+    tearoom = Tearoom(5)
+    art_piece = TearoomGUI(root, tearoom)
+    root.mainloop()
 
-    pygame.init()
-    screen = pygame.display.set_mode((screen_height, screen_width))
-    pygame.display.set_caption('Tearoom')
-    pygame.mouse.set_visible(False)
 
-    background = pygame.Surface(screen.get_size())
-    background = background.convert()
-    background.fill((0, 0, 0))
+class TearoomGUI(tk.Frame):
+    def __init__(self, parent, tearoom):
+        """--documenation.--"""
+        super().__init__(parent)
+        self.parent = parent
+        self.pack(fill=tk.BOTH)
+        self.config(bg=frame_theme['bg'])
+        self.refresh_interval = 100
+        self.tearoom = tearoom
+        self.teacups = tearoom.teacups
+        self.teacup_frames = self._init_teacup_frames()
+        self.teacup_labels = self._init_teacup_labels()
 
-    font = pygame.font.Font(None, 36)
-    text = font.render("Hello There", 1, (10, 10, 10))
+        self._run()
 
-    while True:
+    def _init_teacup_frames(self):
+        frames = []
+        for _ in self.teacups:
+            frames.append(tk.Frame(self))
+        for i in range(len(self.teacups)):
+            frames[i].config(bg=tearoom_theme['fg'])
+        for i in range(self.tearoom.width):
+            for j in range(self.tearoom.width):
+                frames[i * self.tearoom.width + j] \
+                    .grid(row=i, column=j)
+        return frames
 
-        for event in pygame.event.get():
-            if event.type == QUIT:
-                return
-            screen.blit(background, (0, 0))
-            pygame.display.flip()
+    def _init_teacup_labels(self):
+        labels = []
+        for i in range(len(self.teacups)):
+            label = tk.Label(self.teacup_frames[i],
+                             text="",
+                             font=tearoom_theme["font"],
+                             fg=tearoom_theme["fg"],
+                             bg=tearoom_theme["bg"],
+                             width=tearoom_theme["fontwidth"])
+            label.pack()
+            labels.append(label)
+        return labels
 
+    def _run(self):
+        if not self.tearoom.is_alive:
+            return
+        else:
+            self.tearoom.update()
+            self._update()
+            self.after(self.refresh_interval, self._run)
+
+    def _update(self):
+        for i in range(len(self.teacups)):
+            value_repr = ""
+            if self.teacups[i].value:
+                value_repr = str(self.teacups[i].value)
+            self.teacup_labels[i].configure(text=value_repr)
+
+
+black = "#000000"
+neon_green = "#39ff14"
+
+frame_theme = {
+    "bg": black,
+}
+
+tearoom_theme = {
+    "font": ("Helvetica", 84, "bold"),
+    "fg": neon_green,
+    "bg": black,
+    "fontwidth": 3,
+}
 
 if __name__ == '__main__':
     main()
--- tearoom.py
+++ tearoom.py
@@ -7,30 +7,59 @@ class Teacup:
 
     -- Further description. --
     """
-    def __init__(self, increment_rate):
+
+    def __init__(self):
         """Initializes a teacup.
 
-        Attributes:
-        value - current number (1-9) contained in the teacup. None if teacup
-                is empty.
+        value   -- current value (1-9) contained in the teacup. None if teacup
+                   is empty.
         """
         self.value = None
-        self.increment_rate = increment_rate
-        self.timer = 0
+        self._spawn_chance = random.uniform(.0025, .005)
+        self._maturity_rate = random.randint(1, 10)
+        self._timer = 1
 
     def update(self):
         if self.value is None:
-            if random.random() < .1:
+            if self._should_spawn():
                 self.value = 1
-                self.timer = self.increment_rate
         else:
-            if self.timer == self.increment_rate:
+            if self._is_mature():
                 if self.value == 9:
                     self.value = None
-                    self.timer = 0
+                    self._refresh()
                 else:
-                    self.value += 1
-            self.timer += 1
+                    self._increment_value()
+                self._reset_timer()
+            self._increment_timer()
+
+    def _should_spawn(self):
+        """Return True if teacup should start counting, False otherwise."""
+        return random.random() < self._spawn_chance
+
+    def _is_mature(self):
+        """Return True if teacup is mature, False otherwise.
+
+        Used to check if teacup value should be updated.
+        """
+        return self._timer >= self._maturity_rate
+
+    def _reset_timer(self):
+        """Reset the timer."""
+        self._timer = 1
+
+    def _increment_timer(self):
+        """Increment the timer by 1."""
+        self._timer += 1
+
+    def _increment_value(self):
+        """Increment value by 1."""
+        self.value += 1
+
+    def _refresh(self):
+        """Refresh spawn chance and maturity rate."""
+        self._spawn_chance = random.uniform(.005, .02)
+        self._maturity_rate = random.randint(1, 10)
 
 
 class Tearoom:
@@ -38,24 +67,32 @@ class Tearoom:
 
     --Further description. --
     """
-    def __init__(self, size):
-        """Initializes a square tearoom.
+
+    def __init__(self, width):
+        """Initializes a square tearoom and the contained teacups.
 
         Attributes:
 
         -- attribute descriptions. --
         """
+        self.is_alive = True
         self.time_to_live = random.randint(1, sys.maxsize)
-        self.size = size
-        self.max_size = self.size * self.size
+        self.width = width
+        self.teacup_count = self.width * self.width
         self.teacups = []
-        self.init_teacups()
+        self._init_teacups()
 
-    def init_teacups(self):
-        for i in range(self.max_size):
-            teacup = Teacup(random.randint(1, sys.maxsize))
+    def _init_teacups(self):
+        for i in range(self.teacup_count):
+            teacup = Teacup()
             self.teacups.append(teacup)
 
     def update(self):
-        for teacup in self.teacups:
-            teacup.update()
+        if self.is_alive and self.time_to_live == 0:
+            self.is_alive = False
+            for teacup in self.teacups:
+                teacup.value = None
+        else:
+            for teacup in self.teacups:
+                teacup.update()
+            self.time_to_live -= 1
