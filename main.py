bb415394d1faeb62fcc677d46b52bfcda8883989
shoyo-inokuchi <shoyoinokuchi@gmail.com>
2019-01-06 13:10:18 +0900

    finished docstrings

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import time
 import tkinter as tk
 from tearoom import Tearoom
 
@@ -7,29 +6,45 @@ def main():
     """Run the art piece."""
     root = tk.Tk()
     root.title("Tearoom of Zero/One")
-    root.minsize(735, 525)
-    root.geometry("735x525")
-    tearoom = Tearoom(5)
+    root.minsize(798, 574)
+    root.geometry("798x574")
+    tearoom = Tearoom(7)
     art_piece = TearoomGUI(root, tearoom)
     root.mainloop()
 
 
 class TearoomGUI(tk.Frame):
+    """Tkinter GUI for rendition of Tearoom of Zero/One."""
+
     def __init__(self, parent, tearoom):
-        """--documenation.--"""
+        """Create GUI for Tearoom and run the art piece.
+
+        parent           -- parent widget (should be root)
+        tearoom          -- Tearoom instance to display
+        teacups          -- list of Teacups contained in Tearoom
+        teacup_frames    -- list of Frame child widgets for each Teacup
+                            (parent is root)
+        teacup_labels    -- list of Label grandchild widgets for each Teacup's
+                            value (parent is corresponding Frame widget)
+        refresh_interval -- time between each graphical update (milliseconds)
+        """
         super().__init__(parent)
         self.parent = parent
         self.pack(fill=tk.BOTH)
         self.config(bg=frame_theme['bg'])
-        self.refresh_interval = 100
         self.tearoom = tearoom
         self.teacups = tearoom.teacups
         self.teacup_frames = self._init_teacup_frames()
         self.teacup_labels = self._init_teacup_labels()
+        self.refresh_interval = 100
 
         self._run()
 
     def _init_teacup_frames(self):
+        """Create a Frame widget for each Teacup. Return as a list.
+
+        Each widget's parent widget is root.
+        """
         frames = []
         for _ in self.teacups:
             frames.append(tk.Frame(self))
@@ -42,6 +57,11 @@ class TearoomGUI(tk.Frame):
         return frames
 
     def _init_teacup_labels(self):
+        """Create a Label widget for each Teacup's value. Return as a list.
+
+        Each widget's parent widget is the corresponding Teacup's Frame widget.
+        Each parent widget's parent widget is root.
+        """
         labels = []
         for i in range(len(self.teacups)):
             label = tk.Label(self.teacup_frames[i],
@@ -55,6 +75,7 @@ class TearoomGUI(tk.Frame):
         return labels
 
     def _run(self):
+        """Continuously update Tearoom and widgets while Tearoom is alive."""
         if not self.tearoom.is_alive:
             return
         else:
@@ -63,6 +84,7 @@ class TearoomGUI(tk.Frame):
             self.after(self.refresh_interval, self._run)
 
     def _update(self):
+        """Reconfigure each Label widget according to Teacup's value."""
         for i in range(len(self.teacups)):
             value_repr = ""
             if self.teacups[i].value:
@@ -70,6 +92,7 @@ class TearoomGUI(tk.Frame):
             self.teacup_labels[i].configure(text=value_repr)
 
 
+# -- THEMES --
 black = "#000000"
 neon_green = "#39ff14"
 
@@ -78,11 +101,12 @@ frame_theme = {
 }
 
 tearoom_theme = {
-    "font": ("Helvetica", 84, "bold"),
+    "font": ("Helvetica", 64, "bold"),
     "fg": neon_green,
     "bg": black,
     "fontwidth": 3,
 }
+# ----
 
 if __name__ == '__main__':
     main()
--- tearoom.py
+++ tearoom.py
@@ -5,14 +5,20 @@ import random
 class Teacup:
     """An individual teacup that occasionally contains a number.
 
-    -- Further description. --
+    A Teacup can do one of two things: be empty, or be counting up from 1 to 9.
+    Every Teacup starts by being empty. Whether a Teacup starts counting and
+    the pace at which it counts is determined by random chance. After a Teacup
+    reaches 9, it becomes empty once again and restarts the cycle.
     """
 
     def __init__(self):
-        """Initializes a teacup.
+        """Create an empty teacup.
 
-        value   -- current value (1-9) contained in the teacup. None if teacup
-                   is empty.
+        value         -- current value contained in Teacup (1 - 9 or None)
+        spawn_chance  -- probability that empty Teacup will begin counting
+        maturity_rate -- pace at which a Teacup containing a number counts up.
+        timer         -- keeps track of time elapsed. Compared with
+                         maturity_rate to determine whether to increment value.
         """
         self.value = None
         self._spawn_chance = random.uniform(.0025, .005)
@@ -20,18 +26,29 @@ class Teacup:
         self._timer = 1
 
     def update(self):
-        if self.value is None:
+        """Update the value contained in teacup.
+
+        If the Teacup is empty, it can spawn a 1 according to its spawn chance
+        to begin counting. If the Teacup contains a 9, it resets each
+        attribute. Otherwise, it increments its value if the timer has matured.
+        Note that a Teacup's spawn chance and maturity rate is refreshed each
+        time the Teacup resets.
+        """
+        if self._is_empty():
             if self._should_spawn():
                 self.value = 1
         else:
             if self._is_mature():
                 if self.value == 9:
-                    self.value = None
-                    self._refresh()
+                    self._reset()
                 else:
                     self._increment_value()
-                self._reset_timer()
+                    self._reset_timer()
             self._increment_timer()
+
+    def _is_empty(self):
+        """Return True if teacup is empty, False otherwise."""
+        return self.value is None
 
     def _should_spawn(self):
         """Return True if teacup should start counting, False otherwise."""
@@ -40,40 +57,51 @@ class Teacup:
     def _is_mature(self):
         """Return True if teacup is mature, False otherwise.
 
-        Used to check if teacup value should be updated.
+        Used to check if teacup value should be incremented.
         """
         return self._timer >= self._maturity_rate
 
     def _reset_timer(self):
-        """Reset the timer."""
+        """Reset timer."""
         self._timer = 1
 
     def _increment_timer(self):
-        """Increment the timer by 1."""
+        """Increment timer by 1."""
         self._timer += 1
 
     def _increment_value(self):
         """Increment value by 1."""
         self.value += 1
 
+    def _reset(self):
+        """Reset Teacup by emptying and refreshing parameters."""
+        self.value = None
+        self._reset_timer()
+        self._refresh()
+
     def _refresh(self):
         """Refresh spawn chance and maturity rate."""
-        self._spawn_chance = random.uniform(.005, .02)
+        self._spawn_chance = random.uniform(.0025, .005)
         self._maturity_rate = random.randint(1, 10)
 
 
 class Tearoom:
     """A collection of teacups.
 
-    --Further description. --
+    A Tearoom contains a square grid of Teacups. Each contained Teacup behaves
+    completely independently. A Tearoom has a lifespan which is determined
+    randomly when constructed. After a Tearoom "dies", each contained Teacup
+    becomes empty and indefinitely stops counting.
     """
 
     def __init__(self, width):
-        """Initializes a square tearoom and the contained teacups.
+        """Create a square Tearoom.
 
-        Attributes:
-
-        -- attribute descriptions. --
+        is_alive     -- True if Tearoom is alive
+        time_to_live -- number of time steps until Tearoom dies
+        width        -- width of square Tearoom
+        teacup_count -- total number of Teacups in Tearoom
+        teacups      -- list of contained Teacups
         """
         self.is_alive = True
         self.time_to_live = random.randint(1, sys.maxsize)
@@ -83,16 +111,22 @@ class Tearoom:
         self._init_teacups()
 
     def _init_teacups(self):
+        """Initialize Teacups."""
         for i in range(self.teacup_count):
             teacup = Teacup()
             self.teacups.append(teacup)
 
     def update(self):
-        if self.is_alive and self.time_to_live == 0:
-            self.is_alive = False
-            for teacup in self.teacups:
-                teacup.value = None
-        else:
-            for teacup in self.teacups:
-                teacup.update()
-            self.time_to_live -= 1
+        """Update Tearoom and its contained Teacups.
+
+        If the Tearoom is alive, each contained Teacup is updated. Kills
+        Tearoom if time_to_live is 0."""
+        if self.is_alive:
+            if self.time_to_live == 0:
+                self.is_alive = False
+                for teacup in self.teacups:
+                    teacup.value = None
+            else:
+                for teacup in self.teacups:
+                    teacup.update()
+                self.time_to_live -= 1
